6484113def11c70f2d773817eab168695b15e3ba
Ernesto Rocha <ernestoar2301@gmail.com>
2023-09-19 23:46:56 -0600

    Update mapa.tsx

--- src/app/(users)/doctor/dashboard/mapa.tsx
+++ src/app/(users)/doctor/dashboard/mapa.tsx
@@ -1,5 +1,7 @@
 import { Avatar, Card, CardBody, CardHeader } from '@nextui-org/react'
 import React from 'react'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 interface Medication {
     quantity: number,
@@ -38,10 +40,30 @@ export const Mapa = () => {
                     <h3 className='text-center text-lg font-medium text-gray-500 w-full'>Most prescribed medications</h3>
                 </CardHeader>
                 <div className="flex flex-col gap-2 mt-5">
-                     <h1>Hola aqui va el mapa</h1>
-                     <br /> <br /> <br /> <br /> <br /> <br /> <br />
+                    <h1>Hola aquí va el mapa</h1>
+
+                    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "300px", width: "100%" }}>
+  <TileLayer
+    url="https://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png"
+    maxZoom={19}
+  />
+  <Marker position={[51.505, -0.09]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
+
+
+
                 </div>
             </CardBody>
         </Card>
+
+
+
+
+
+
     )
 }
