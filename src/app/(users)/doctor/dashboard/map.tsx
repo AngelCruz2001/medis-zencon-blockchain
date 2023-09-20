e5d2fa590b4d681473e8c2d3342c1ee19df0d8c8
RickSandov <i.s.ricardo.sandoval@gmail.com>
2023-09-20 01:49:56 -0600

    update

--- src/app/(users)/doctor/dashboard/map.tsx
+++ src/app/(users)/doctor/dashboard/map.tsx
@@ -15,35 +15,6 @@ const locationIcon = L.icon({
 
 export const Map = () => {
     return (
-<<<<<<< HEAD
-        <Card className='w-full'>
-            <CardBody>
-                <CardHeader>
-                    <h3 className='text-center text-lg font-medium text-gray-500 w-full'>Most prescribed medications</h3>
-                </CardHeader>
-                <div className="flex flex-col gap-2 mt-5">
-                    <h1>Hola aquí va el mapa</h1>
-
-                    <MapContainer center={[-22.9866, -43.1982]} zoom={13} style={{ height: "300px", width: "100%" }}>
-                        <TileLayer
-                            url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
-                            maxZoom={19}
-                        />
-                        <Circle
-                            center={[-22.9866, -43.1982]}
-                            pathOptions={{ color: 'red', fillColor: '#f03', fillOpacity: 0.5 }}
-                            radius={500}>
-                            <Popup>
-                                I am a circle.
-                            </Popup>
-                        </Circle>
-                        <Marker position={[-22.9866, -43.1982]} icon={locationIcon}>
-                            <Popup>
-                                A pretty CSS3 popup. <br /> Easily customizable.
-                            </Popup>
-                        </Marker>
-                    </MapContainer>
-=======
         <div className="w-full">
             <h1 className="text-xl font-semibold mt-3 mb-2">Appointments</h1>
             <Card className='w-full mb-4'>
@@ -68,7 +39,6 @@ export const Map = () => {
                                 </Popup>
                             </Marker>
                         </MapContainer>
->>>>>>> c915d8c15699e7c02636ca46673e5d74dd9cce65
 
 
 
