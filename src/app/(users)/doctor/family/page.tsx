7df89017ab9eb13025fbb9c6750659a125423d97
angi_ed01@hotmail.com <angi_ed01@hotmail.com>
2023-09-20 07:38:22 -0600

    Arbol genealogico fix on production 02

--- src/app/(users)/doctor/family/page.tsx
+++ src/app/(users)/doctor/family/page.tsx
@@ -29,7 +29,7 @@ const GraphPage: React.FC = () => {
   });
 
   const fetchData = async () => {
-    const resp = await fetch("https://medis-zencon-blockchain-gbs40p932-angied01.vercel.app/api/family");
+    const resp = await fetch("http://localhost:3000/api/family");
     const data = await resp.json();
     setExampleData(data);
   };
